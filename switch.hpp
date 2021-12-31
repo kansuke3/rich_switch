5f64c8ceb82dada9cd7b17f315c10bee21a01e54
kansuke3 <69252354+kansuke3@users.noreply.github.com>
2022-01-01 05:00:54 +0900

    add add_default and update match

--- switch.hpp
+++ switch.hpp
@@ -7,9 +7,14 @@ class string_switch{
 		void add(std::string s,std::function<void(void)> f){
 			cases.emplace(s,f);
 		}
+		void add_default(std::function<void(void)> f){
+			default_case=f;
+		}
 		auto match(std::string s){
-			return cases[s];
+			if(cases[s]!=nullptr)return cases[s];
+			return default_case;
 		}
 	private:
 		std::unordered_map<std::string,std::function<void(void)>> cases;
+		std::function<void(void)> default_case;
 };
